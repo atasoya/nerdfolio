91efe8c826bc38156ba22233a18e61a27fd93594
Ata Atasoy <ataatasoy2013@gmail.com>
2025-10-26 21:34:57 +0100

    wrong function name

--- internal/app/nerdfolio/commands/build.go
+++ internal/app/nerdfolio/commands/build.go
@@ -9,7 +9,7 @@ import (
 var BuildCmd = flag.NewFlagSet("build", flag.ExitOnError)
 var BuildColorScheme = BuildCmd.String("color", "tokyonight", "Color Scheme")
 
-func HandleBuild() {
+func HandleBuildCommand() {
 	BuildCmd.Parse(os.Args[2:])
 	fmt.Println("subcommand 'build'")
 	fmt.Println("  color-scheme:", *BuildColorScheme)
