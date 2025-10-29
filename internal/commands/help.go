579d95b346f9a1fc1e8da7d95348460fcd89f12b
Ata Atasoy <ataatasoy2013@gmail.com>
2025-10-29 11:07:11 +0100

    help command

--- internal/commands/help.go
+++ internal/commands/help.go
@@ -9,6 +9,25 @@ import (
 var HelpCmd = flag.NewFlagSet("help", flag.ExitOnError)
 
 func HandleHelpCommand() {
+	ShowHelp()
+}
+
+func ShowHelp() {
 	HelpCmd.Parse(os.Args[2:])
-	fmt.Println("help command")
+
+	var Blue = "\033[34m"
+	var Magenta = "\033[35m"
+	var Reset = "\033[0m"
+
+	fmt.Println(Blue+"\nnerdfolio"+Reset, "0.1")
+
+	fmt.Println(Blue+"USAGE:", Reset)
+	fmt.Println("	nerdfolio [OPTIONS]\n")
+
+	fmt.Println(Blue+"OPTIONS:", Reset)
+	fmt.Println(Magenta, "	build <COLOR_SCHEME>", Reset)
+	fmt.Println("		Build your source code")
+
+	fmt.Println(Magenta, "	help", Reset)
+	fmt.Println("		Print help information")
 }
