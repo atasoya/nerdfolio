fd61876041a82e2d0ba161ff7e081f041af16abe
Ata Atasoy <ataatasoy2013@gmail.com>
2025-10-30 20:08:13 +0100

    multiple html file support

--- internal/commands/build.go
+++ internal/commands/build.go
@@ -14,26 +14,26 @@ import (
 var BuildCmd = flag.NewFlagSet("build", flag.ExitOnError)
 var BuildColorSchemeFlag = BuildCmd.String("colorScheme", "catppuccinMocha", "Color Scheme")
 
+var currentPath string
+
 func HandleBuildCommand() {
 	start := time.Now()
 	BuildCmd.Parse(os.Args[2:])
 	fmt.Println("Starting the build process!")
 
-	currentPath, _ := os.Getwd()
+	currentPath, _ = os.Getwd()
 	outputDirectory := currentPath + "/out"
 
 	createOutDirectoryIfNotExsists(outputDirectory)
 
-	indexHtmlContentData, _ := os.ReadFile(currentPath + "/index.html")
-	indexHtmlContent := string(indexHtmlContentData)
-
-	indexHtmlContent = replaceTemplates(indexHtmlContent, currentPath)
-
-	err := os.WriteFile(currentPath+"/out/index.html", []byte(indexHtmlContent), 0644)
+	htmlFilesMap := createHtmlFilesMap()
+	htmlFilesMap, err := replaceTemplates(htmlFilesMap, currentPath)
 	if err != nil {
-		fmt.Println("error while writing index.html content")
+		fmt.Println("Error replacing templates:", err)
 		os.Exit(1)
 	}
+
+	writeHtmlFilesToOutDirectory(htmlFilesMap)
 
 	flags.HandleColorSchemeFlag(BuildColorSchemeFlag, currentPath)
 
@@ -43,28 +43,66 @@ func HandleBuildCommand() {
 
 }
 
-func replaceTemplates(indexHtmlContent string, currentPath string) string {
-	templateMap := make(map[string]string)
-	files, err := os.ReadDir("templates/")
+func replaceTemplates(htmlFilesMap map[string]string, currentPath string) (map[string]string, error) {
+	templateDir := filepath.Join(currentPath, "templates")
+	entries, err := os.ReadDir(templateDir)
 	if err != nil {
-		fmt.Println("Error while reading templates")
-		os.Exit(1)
+		return nil, fmt.Errorf("reading templates: %w", err)
 	}
 
-	for _, file := range files {
+	templateMap := make(map[string]string)
+	for _, file := range entries {
+		if file.IsDir() {
+			continue
+		}
 		fileName := file.Name()
-		baseName := strings.TrimSuffix(file.Name(), filepath.Ext(file.Name()))
-
-		data, err := os.ReadFile(currentPath + "/templates/" + fileName)
+		baseName := strings.TrimSuffix(fileName, filepath.Ext(fileName))
+		data, err := os.ReadFile(filepath.Join(templateDir, fileName))
 		if err != nil {
-			fmt.Println("Error while reading html files")
-			os.Exit(1)
+			return nil, fmt.Errorf("reading template %s: %w", fileName, err)
 		}
 		templateMap[baseName] = string(data)
-		re := regexp.MustCompile(`\{%\s*` + baseName + `\s*%\}`)
-		indexHtmlContent = re.ReplaceAllString(indexHtmlContent, templateMap[baseName])
 	}
-	return indexHtmlContent
+
+	for htmlFile, content := range htmlFilesMap {
+		for name, tmpl := range templateMap {
+			re := regexp.MustCompile(fmt.Sprintf(`\{%%\s*%s\s*%%\}`, regexp.QuoteMeta(name)))
+			content = re.ReplaceAllString(content, tmpl)
+		}
+		htmlFilesMap[htmlFile] = content
+	}
+
+	return htmlFilesMap, nil
+}
+
+func createHtmlFilesMap() map[string]string {
+	htmlFilesMap := make(map[string]string)
+	entries, _ := os.ReadDir(currentPath)
+	for _, entry := range entries {
+		if entry.IsDir() {
+			continue
+		}
+
+		fileName := entry.Name()
+
+		if !strings.HasSuffix(strings.ToLower(fileName), ".html") {
+			continue
+		}
+
+		filePath := filepath.Join(currentPath, fileName)
+
+		data, err := os.ReadFile(filePath)
+		if err != nil {
+			fmt.Println("Error reading file:", fileName, err)
+			os.Exit(1)
+		}
+
+		htmlFilesMap[fileName] = string(data)
+
+	}
+
+	return htmlFilesMap
+
 }
 
 func createOutDirectoryIfNotExsists(outputDirectory string) {
@@ -72,6 +110,17 @@ func createOutDirectoryIfNotExsists(outputDirectory string) {
 		err := os.Mkdir("out", 0755)
 		if err != nil {
 			fmt.Println("There was a problem creating /out directry")
+			os.Exit(1)
+		}
+	}
+}
+
+func writeHtmlFilesToOutDirectory(htmlFilesMap map[string]string) {
+	for htmlFile, htmlFileContent := range htmlFilesMap {
+
+		err := os.WriteFile(currentPath+"/out/"+htmlFile, []byte(htmlFileContent), 0644)
+		if err != nil {
+			fmt.Println("error while writing index.html content")
 			os.Exit(1)
 		}
 	}
